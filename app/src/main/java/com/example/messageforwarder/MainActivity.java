cc6b8522cbad5147d75b9ad2d2085951c7b97daa
mr.infinidy <afkdev8@gmail.com>
2021-04-17 02:39:50 +0200

    first steps top bar design

--- app/src/main/java/com/example/messageforwarder/MainActivity.java
+++ app/src/main/java/com/example/messageforwarder/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.messageforwarder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get screen size
+        int screenHeight = getScreenHeight();
+        int screenWidth = getScreenWidth();
+
+        //display layout
+        TextView topBar = (TextView)findViewById(R.id.topBar);
+        RelativeLayout.LayoutParams topBarParams = (RelativeLayout.LayoutParams)topBar.getLayoutParams();
+        topBarParams.height = screenHeight / 6;
+        topBar.setTextSize(screenHeight/(float)60);
+    }
+
+    private int getScreenHeight() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        return metrics.heightPixels;
+    }
+
+    private int getScreenWidth() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        return metrics.widthPixels;
     }
 }
